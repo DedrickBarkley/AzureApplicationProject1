22ca97d386d551ec36ff7eb006730a31fd31664a
DedrickBarkley <Dedrick_barkley@yahoo.com>
2025-02-05 09:44:13 -0600

    Update config.py
    update configs

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '30a24fd4-ce5d-4126-963f-2cd90f8f2da9'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmss'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'enter key'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmss.database.windows.net'
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "enter secret"
+    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -30,10 +30,9 @@ class Config(object):
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "b2c3874c-bda6-44b8-8d34-92a4c640e9e8"
+    CLIENT_ID = "5b4830dd-07cc-458d-b981-14751052ee7a"
 
-    #REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
-    REDIRECT_PATH = "https://udacitycmss.azurewebsites.net/getAToken"
+    REDIRECT_PATH = "/authorized"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
